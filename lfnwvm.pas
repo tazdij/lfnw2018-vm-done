5fdec26c0932dd1b9ed76ea6f01e64040ff0e7fc
Don Duvall <dduvall@psft.local>
2018-04-24 13:54:53 -0700

    More ops?

--- lfnwvm.pas
+++ lfnwvm.pas
@@ -178,8 +178,13 @@ begin
 
 end;
 
+procedure VM_OpARG_RI(state : PVMState);
+begin
+
+end;
+
 (* CALL label_to_goto numargs *)
-(* CALL x0F000000 x02 *)
+(* CALL x0F000000 x02000000 *)
 procedure VM_OpCALL_A(state : PVMState);
 var destAddr : LongInt = 0;
     retAddr : LongInt = 0;
@@ -189,18 +194,25 @@ begin
   // All arguments should have been pushed by caller already
 
   // Return address is next instruction
-  retAddr := state^.PC + 5;
+  //        OP[1] ADDR[4] ARGS[4]
+  Move(state^.PM[state^.PC + 9], retAddr, 4);
 
-  Move(state^.PM[state^.PC + 1], state^.PC, 4); // Move the address to call into PC
-
-  // Put the return address on the Stack
-  Move(retAddr, state^.SM[state^.SP], 4);
-  state^.SP := state^.SP + 4;
+  // Put number of args on stack
+  Move(state^.PM[state^.PC + 5], state^.SM[state^.SP], 4);
+  Inc(state^.SP, 4);
 
   // Put the previous Frame Pointer on the Stack
   Move(state^.FP, state^.SM[state^.SP], 4);
+  Inc(state^.SP, 4);
+
+  // Put the return address on the Stack
+  Move(retAddr, state^.SM[state^.SP], 4);
+  Inc(state^.SP, 4);
+
   state^.FP := state^.SP;
-  state^.SP := state^.SP + 4;
+
+  // Move the address to call into PC
+  Move(state^.PM[state^.PC + 1], state^.PC, 4);
 
 end;
 
@@ -216,21 +228,12 @@ begin
     04 - arg2
     ---------------------------
     08 - Number of Params
-    08 - return address (PC)
-    0C - frame index (-1)       <--- FP = 08
+    0C - frame index (-1)
+    10 - return address (PC)
     ... LOCALS ...
     10 - arg1
     14 - arg2
     ---------------------------
-    18 - return address (PC)
-    1C - frame index (0C)       <--- FP = 1C
-    ... LOCALS ...
-    20 - arg1
-    24 - arg2
-    ---------------------------
-    28 - return address (PC)
-    2C - frame index (1C)       <--- FP = 2C
-    30 - ______________         <--- SP = 30
     ...
 
     FP register contains address of the start of the current Stack Frame
@@ -362,6 +365,8 @@ begin
 
   VM_RegisterOpHandler(state, 10, @VM_OpPUSH_Il);
 
+  (* Stack Arguments *)
+  VM_RegisterOpHandler(state, 90, @VM_OpARG_RI);
 
   (* Stack Function Calls *)
   VM_RegisterOpHandler(state, 100, @VM_OpCALL_A);
